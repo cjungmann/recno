eb5955d3f3d058642eb5d5485a8ba9d2228630ca
Chuck Jungmann <chuck@cpjj.net>
2021-06-11 15:30:11 -0500

    New inlines, made all inlines "static inline".

--- src/extra.h
+++ src/extra.h
@@ -4,28 +4,40 @@
 #define EXTRA_H
 
 #include <stdbool.h>
+#include <assert.h>
 #include "blocks.h"   // for MAGIC_STRING
 
-inline HEAD_HANDLE* head_handle_from_db_handle(DB_HANDLE *h)   { return &h->head_handle; }
-inline FILE* file_from_db_handle(DB_HANDLE *h)          { return h->file; }
+#define INLINE static inline
 
-inline const HEAD_HANDLE* const_head_handle_from_db_handle(const DB_HANDLE *h)
+INLINE HEAD_HANDLE* head_handle_from_db_handle(DB_HANDLE *h)   { return &h->head_handle; }
+INLINE FILE* file_from_db_handle(DB_HANDLE *h)    { return h->file; }
+
+INLINE HEAD_FILE *hh_header(HEAD_HANDLE *hh)      { return &hh->header; }
+INLINE BTYPE hh_type(HEAD_HANDLE *hh)             { return hh->header.info_block.type; }
+
+INLINE bool hh_is_chain_block(HEAD_HANDLE *hh)    { return hh_type(hh) >= RN_CHAIN; }
+INLINE bool hh_is_table_block(HEAD_HANDLE *hh)    { return hh_type(hh) >= RN_TABLE; }
+INLINE bool hh_is_file_block(HEAD_HANDLE *hh)     { return hh_type(hh) >= RN_FILE; }
+
+INLINE INFO_BLOCK *hh_info_block(HEAD_HANDLE *hh) { return &hh->header.info_block; }
+INLINE INFO_CHAIN *hh_info_chain(HEAD_HANDLE *hh) { return &hh->header.info_chain; }
+INLINE INFO_TABLE *hh_info_table(HEAD_HANDLE *hh) { return &hh->header.info_table; }
+
+INLINE bool hh_file_good_magic(HEAD_HANDLE *hh)
 {
-   return &h->head_handle;
+   return ! strncmp(hh->header.info_file.magic, MAGIC_STRING, 4);
 }
 
-inline bool confirm_head_file(const HEAD_FILE *head_file)
+INLINE off_t *hh_block_penultimate(HEAD_HANDLE *hh)
 {
-   return head_file
-      && head_file->info_block.type == RN_FILE
-      && head_file->info_block.size_head == recno_get_headsize(RN_FILE)
-      && !strncmp(head_file->info_file.magic, MAGIC_STRING, 4);
+   assert(hh_is_chain_block(hh));
+   return &hh_info_chain(hh)->block_penultimate;
 }
 
-inline bool confirm_head_handle(const HEAD_HANDLE *head_handle)
+INLINE off_t *hh_block_last(HEAD_HANDLE *hh)
 {
-   return confirm_head_file(&head_handle->head_file);
+   assert(hh_is_chain_block(hh));
+   return &hh_info_chain(hh)->block_last;
 }
-
 
 #endif
