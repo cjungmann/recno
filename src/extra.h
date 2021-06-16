35e3c54734d8b968ddf7c3adcf498729770cf6a5
Chuck Jungmann <chuck@cpjj.net>
2021-06-16 09:25:38 -0500

    Added several head member access functions.

--- src/extra.h
+++ src/extra.h
@@ -22,22 +22,45 @@ INLINE bool hh_is_file_block(HEAD_HANDLE *hh)     { return hh_type(hh) >= RN_FIL
 INLINE INFO_BLOCK *hh_info_block(HEAD_HANDLE *hh) { return &hh->header.info_block; }
 INLINE INFO_CHAIN *hh_info_chain(HEAD_HANDLE *hh) { return &hh->header.info_chain; }
 INLINE INFO_TABLE *hh_info_table(HEAD_HANDLE *hh) { return &hh->header.info_table; }
+INLINE INFO_FILE  *hh_info_file(HEAD_HANDLE *hh)  { return &hh->header.info_file; }
 
 INLINE bool hh_file_good_magic(HEAD_HANDLE *hh)
 {
    return ! strncmp(hh->header.info_file.magic, MAGIC_STRING, 4);
 }
 
-INLINE off_t *hh_block_penultimate(HEAD_HANDLE *hh)
+INLINE uint16_t hh_size_head(HEAD_HANDLE *hh) { return hh_info_block(hh)->size_head; }
+INLINE uint32_t hh_size_block(HEAD_HANDLE *hh) { return hh_info_block(hh)->size_block; }
+INLINE BLOC *hh_next_block(HEAD_HANDLE *hh) { return &hh_info_block(hh)->next_block; }
+
+INLINE off_t hh_block_penultimate(HEAD_HANDLE *hh)
 {
    assert(hh_is_chain_block(hh));
-   return &hh_info_chain(hh)->block_penultimate;
+   return hh_info_chain(hh)->block_penultimate;
 }
 
-INLINE off_t *hh_block_last(HEAD_HANDLE *hh)
+INLINE off_t hh_block_last(HEAD_HANDLE *hh)
 {
    assert(hh_is_chain_block(hh));
-   return &hh_info_chain(hh)->block_last;
+   return hh_info_chain(hh)->block_last;
+}
+
+INLINE uint32_t hh_rec_size(HEAD_HANDLE *hh)
+{
+   assert(hh_is_table_block(hh));
+   return hh_info_table(hh)->rec_size;
+}
+
+INLINE uint32_t hh_last_recno(HEAD_HANDLE *hh)
+{
+   assert(hh_is_table_block(hh));
+   return hh_info_table(hh)->last_recno;
+}
+
+INLINE uint32_t hh_chunk_size(HEAD_HANDLE *hh)
+{
+   assert(hh_is_file_block(hh));
+   return hh_info_file(hh)->chunk_size;
 }
 
 #endif
